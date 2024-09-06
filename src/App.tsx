7daf2daeafd0a20a5357d5457051c3941f4f2c71
Leandro Ruel <leandro.ruel@gmail.com>
2024-09-05 22:55:56 -0300

    chore: clean up this component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { Editor, Element } from "@craftjs/core";
 import "./App.css";
-import { Text, Row, Col, DataTable } from "./components/user";
-import { Viewport } from "./components/editor/Viewport";
-import { RenderNode } from "./components/editor/RenderNode";
-import Canvas from "./components/editor/Canvas";
 
 function App() {
   return (
     <>
-      <Editor resolver={{ Text, Row, Col, Element, Canvas, DataTable }} onRender={RenderNode}>
-        <Viewport />
-      </Editor>
+     <h1>welcome to the page creator</h1>
     </>
   );
 }
